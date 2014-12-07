5e22473d626cc13ed7e320219d33e11d64fd1731
Nerea <neri30rv@gmail.com>
2014-12-06 20:37:05 -0600

    Clase15

---
+++ Clases/Clase15/src/clase15/Clase15.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase15;
+
+import Frontend.FrmMain;
+
+/**
+ *
+ * @author Nerea
+ */
+public class Clase15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmMain oFrmMain=new FrmMain();
+        oFrmMain.setVisible(true);
+        
+    }
+    
+}
